4df165574dcc425dd5484bb57ee1e9a0ff7361ec
Leng Trang <lengtrang@Ios.eccrf.ford.com>
2017-01-12 13:48:42 -0500

    My First Commit

---
+++ HelloSDL/HelloSDL/HSDLHMIBuilder.h
@@ -0,0 +1,13 @@
+//
+//  HSDLHMIBuilder.h
+//  HelloSDL
+//
+//  Created by Leng Trang on 1/12/17.
+//  Copyright © 2017 Ford. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HSDLHMIBuilder : NSObject
+
+@end
---
+++ HelloSDL/HelloSDL/HSDLHMIBuilder.m
@@ -0,0 +1,13 @@
+//
+//  HSDLHMIBuilder.m
+//  HelloSDL
+//
+//  Created by Leng Trang on 1/12/17.
+//  Copyright © 2017 Ford. All rights reserved.
+//
+
+#import "HSDLHMIBuilder.h"
+
+@implementation HSDLHMIBuilder
+
+@end
